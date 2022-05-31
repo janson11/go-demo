bec2b33b118d3ec8d4a8317197cfb88228c36242
shanjian <shanjian@codemao.com>
2022-05-31 09:53:59 +0800

    结构体

---
+++ src/part6/main.go
@@ -0,0 +1,50 @@
+package main
+
+import "fmt"
+
+func main() {
+	personInit()
+
+}
+
+func NewPerson(name string) *person {
+	return &person{name:name}
+}
+
+func (addr address) String() string {
+	return fmt.Sprintf("the addr is %s%s", addr.province, addr.city)
+}
+
+func printString(s fmt.Stringer) {
+	fmt.Println(s.String())
+}
+
+func (p person) String() string {
+	return fmt.Sprintf("the name is %s,age is %d", p.name, p.age)
+}
+
+func personInit() {
+	p := person{
+		age:  31,
+		name: "janson",
+		addr: address{
+			province: "广东省",
+			city:     "深圳市",
+		},
+	}
+	fmt.Println(p.addr)
+	printString(p)
+}
+
+type person struct {
+	name string
+	age  uint
+	addr address
+}
+
+type address struct {
+	province string
+	city     string
+}
+
+
